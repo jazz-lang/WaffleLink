15e7ef5afbbea5d483c651b79c9df621742d880b
Adel Prokurov <adel.prokurov@protonmail.com>
2020-03-24 10:22:03 +0300

    StringCell::hash

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,44 @@
-fn main() {}
+/*
+*   Copyright (c) 2020 Adel Prokurov
+*   All rights reserved.
+
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+
+*   http://www.apache.org/licenses/LICENSE-2.0
+
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+#[macro_use]
+extern crate waffle;
+
+use waffle::runtime::cell::*;
+extern "C" {
+    fn malloc(size: usize) -> *mut u8;
+}
+
+use std::mem::size_of;
+
+fn main() {
+    unsafe {
+        let mem = malloc(size_of::<StringCell>() + "Hello,World!\0".len()) as *mut Cell;
+        (&mut *mem).forward = waffle::util::mem::Address::from(42);
+        let string = (&*mem).cast::<StringCell>();
+        std::ptr::copy_nonoverlapping(
+            b"Hello,World\0".as_ptr(),
+            string.offset(waffle::offset_of!(StringCell, value) as isize) as *mut u8,
+            "Hello,World!\0".len(),
+        );
+        extern "C" {
+            fn puts(raw: *mut u8);
+        }
+        puts(string.offset(offset_of!(StringCell, value) as isize) as *mut u8);
+        println!("{}", (&*string).header.forward.to_usize());
+    }
+}
--- src/runtime/cell.rs
+++ src/runtime/cell.rs
@@ -1,3 +1,20 @@
+/*
+*   Copyright (c) 2020 Adel Prokurov
+*   All rights reserved.
+
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+
+*   http://www.apache.org/licenses/LICENSE-2.0
+
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
 use super::arc::*;
 use super::value::*;
 
@@ -22,31 +39,82 @@ impl Cell {
 pub struct StringCell {
     pub header: Cell,
     pub length: usize,
-    pub hash: usize,
+    pub hash: u32,
     pub value: *mut u8,
+}
+
+impl StringCell {
+    pub fn length(&self) -> usize {
+        unsafe { *((self as *const Self).offset(offset_of!(Self, length) as isize) as *mut usize) }
+    }
+    pub fn length_slot(&self) -> *mut usize {
+        unsafe { (self as *const Self).offset(offset_of!(Self, length) as isize) as *mut usize }
+    }
+
+    pub fn hash_slot(&self) -> *mut u32 {
+        unsafe { (self as *const Self).offset(offset_of!(Self, hash) as isize) as *mut u32 }
+    }
+
+    pub unsafe fn set_length(&self, len: usize) {
+        let ptr = unsafe {
+            &mut *((self as *const Self).offset(offset_of!(Self, length) as isize) as *mut usize)
+        };
+        *ptr = len;
+    }
+
+    pub fn value<'a>(&self) -> &'a str {
+        unsafe {
+            let ptr = (self as *const Self).offset(offset_of!(Self, value) as isize) as *mut u8;
+            let bytes = std::slice::from_raw_parts(ptr, self.length());
+
+            std::str::from_utf8(bytes).unwrap()
+        }
+    }
+
+    pub fn hash(&self) -> u32 {
+        unsafe {
+            let ptr = (self as *const Self).offset(offset_of!(Self, hash) as isize) as *mut u32;
+            let hash = *ptr;
+            if hash == 0 {
+                *ptr = fxhash::hash32(self.value())
+            }
+
+            *ptr
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    extern "C" {
+        fn malloc(s: usize) -> *mut u8;
+    }
     use super::*;
+    use std::io::Write;
+    use std::mem::size_of;
     #[test]
     fn alloc_string() {
         unsafe {
-            let scell_layout = std::alloc::Layout::new::<StringCell>();
-            let mem = std::alloc::alloc(
-                std::alloc::Layout::from_size_align(scell_layout.size() + 14, scell_layout.align())
-                    .unwrap(),
-            ) as *mut Cell;
+            let mem = malloc(size_of::<StringCell>() + "Hello,World!\0".len()) as *mut Cell;
+            (&mut *mem).forward = crate::util::mem::Address::from(42);
             let string = (&*mem).cast::<StringCell>();
+            (*string).set_length("Hello,World!".len());
             std::ptr::copy_nonoverlapping(
-                b"Hello,World!".as_ptr(),
-                (&mut *string).value,
-                "Hello,World!".len(),
+                b"Hello,World!\0".as_ptr(),
+                string.offset(crate::offset_of!(StringCell, value) as isize) as *mut u8,
+                "Hello,World!\0".len(),
             );
             extern "C" {
                 fn puts(raw: *mut u8);
             }
-            puts((&*string).value);
+            puts(string.offset(offset_of!(StringCell, value) as isize) as *mut u8);
+            assert_eq!((&*string).header.forward.to_usize(), 42);
+            std::io::stderr()
+                .lock()
+                .write(format!("{}\n", (*string).length()).as_bytes())
+                .unwrap();
+            assert_eq!((*string).length(), 12);
+            assert_eq!((*string).value(), "Hello,World!");
         }
     }
 }
