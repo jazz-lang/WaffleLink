3ad5dd3a988b8289c6731434358b13cc12c94e9f
playX <gtashnik11@gmail.com>
2020-09-09 09:30:29 +0300

    Value encoding

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,39 @@
 #![allow(non_snake_case)]
 #![allow(unused_mut, dead_code, unused_variables)]
 
+#[macro_export]
+macro_rules! lohi_struct {
+    (struct $name : ident {
+        $field1: ident : $t: ty,
+        $field2: ident : $t2: ty,
+    }) => {
+        #[derive(Copy, Clone, PartialEq, Eq)]
+        #[repr(C)]
+        #[cfg(target_endian = "big")]
+        pub struct $name {
+            pub $field2: $t2,
+            pub $field1: $t,
+        }
+        #[derive(Copy, Clone, PartialEq, Eq)]
+        #[repr(C)]
+        #[cfg(target_endian = "little")]
+        pub struct $name {
+            pub $field1: $t,
+            pub $field2: $t,
+        }
+    };
+}
+#[macro_export]
+macro_rules! unused {
+    ($($var: ident),*) => {
+        $(
+            let _ = $var;
+        )*
+    };
+}
 pub mod gc;
 pub mod heap;
 pub mod timer;
 pub mod utils;
+pub mod values;
 pub mod vm;
